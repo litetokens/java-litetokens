e61a8200b6b39e94679f4b1f3d199a2de3ed9df6
ashu <ithinker1991@gmail.com>
2018-06-08 15:35:33 +0800

    forge block

--- src/main/java/org/tron/core/services/WitnessService.java
+++ src/main/java/org/tron/core/services/WitnessService.java
@@ -134,7 +134,7 @@ public class WitnessService implements Service {
   private BlockProductionCondition forgeBlock() {
 
     long when = tronApp.getDbManager().getDynamicPropertiesStore().getLatestBlockHeaderTimestamp();
-    int num = 2000_000;
+    int num = 3000_000;
 
     if (when == 0) {
       when = DateTime.now().getMillis() - (num * ChainConstant.BLOCK_PRODUCED_INTERVAL);

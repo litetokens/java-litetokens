ec84bd65e997ca1acf623ee1d97579432f68a043
ashu <ithinker1991@gmail.com>
2018-09-20 17:31:15 +0800

    add vote info

--- src/main/java/org/tron/program/FullNode.java
+++ src/main/java/org/tron/program/FullNode.java
@@ -87,19 +87,43 @@ public class FullNode {
       manager.getWitnessStore().delete(witnessCapsule.getAddress().toByteArray());
     });
     String[] newAccount = {
-        "TAbzgkG8p3yF5aywKVgq9AaAu6hvF2JrVC",
         "TRx2rc1v91HjUFdeBBgNSiqirctq94sAfA",
         "TRxETQim3Jn5TYqLeAnpyF5XdQeg7NUcSJ",
-         "TRxUu1ZhEYsZw9AHyg8gXBmRSmUzaZPWaw",
-         "TRxgBU7HFTQvU6zPheLHphqpwhDKNxB6Rk",
-         "TRxscEvPTPFaCxBMuFVmzEybzZWJZM9eAB",
-         "TRx32uh7TQjdnLFKyWVPKJBfEn1XWjJtcm"};
+        "TRxUu1ZhEYsZw9AHyg8gXBmRSmUzaZPWaw",
+        "TRxgBU7HFTQvU6zPheLHphqpwhDKNxB6Rk",
+        "TRxscEvPTPFaCxBMuFVmzEybzZWJZM9eAB",
+        "TRx32uh7TQjdnLFKyWVPKJBfEn1XWjJtcm",
+        "TRxF8fZERk4XzQZe1SzvkS5nyNJ7x6tGZ5",
+        "TRxUztFKWdXy42MSdiHQoef5VLaXADMJp3",
+        "TRxh1GnspMRadaU37UzrRRpkME2EkwCHg4",
+        "TRxsiQ2vugWqY2JGr39NHqysAw5zHfWhpU",
+        "TRx3MZDxWzTBW3HYX3ZWGEBrvAC8upGA8C",
+        "TRxFANjAvztBibiqPRWgG841fVP12BCH7d",
+        "TRxVs5MRUy2yHn2kqwev81VjYwXBdYdXrD",
+        "TRxhePptGctYfCpxFCsLLAHUr1iShFkGC1",
+        "TRxtaoGBJeSwQJu5551cBhaw5sW3vaazuF",
+        "TRx3cWa892UxbCaoqCjidp3r946SLZ6U72",
+        "TRxFZ7TDgQGF8MfLxnjQ9EqL5WtEiUmTmH",
+        "TRxVyqGWNwiPCetP7EQTnukdsVGgMpXzwj",
+        "TRxinhH2wZa4zPCqgcUgEZTx3uYs9bFKuM",
+        "TRxtfixDf8e4MnZw6zRAVbL3isVnnaiq2o",
+        "TRx4sTiyZuDN8whJUyovHZNTk6UYdsqqwg",
+        "TRxFiLJp8i5YMQyG2rJFzNA9htaTc7wLcf",
+        "TRxXnVabXh8QzdPvAGigmyuYuC391hzmwL",
+        "TRxiyR3cJPwyMMpq3WQQF7xiRkNDLkyd9X",
+        "TRxu36iquybaSti8ZhVzZ2tPgK7NiXTrSn",
+        "TRx4znAxu5FWxb5ccVUX89TtZ8qWF2PM2b",
+        "TRxYCcQNn7U7RtN7ZqF36GQYhfMTKnoarw"
+    };
 
     int idx = 0;
     for (String acc: newAccount) {
       byte[] address = Wallet.decodeFromBase58Check(acc);
       AccountCapsule account = new AccountCapsule(ByteString.copyFrom(address), AccountType.Normal);
-      account.setBalance(1000000000000000000L);
+      account.setBalance(9000000000000000000L);
+      account.addVotes(ByteString.copyFrom(address), 1_000_000_000_000L);
+      account.setFreeNetUsage(1_000_000_000_000L);
+      account.setFrozen(1_000_000_000_000L, 10000);
       context.getBean(Manager.class).getAccountStore().put(address, account);
       manager.insertWitness(address, idx++);
     }

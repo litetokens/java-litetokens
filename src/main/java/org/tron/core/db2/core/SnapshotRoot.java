9d94b698fb5a1179ab11783ef9eb5a253489c67b
ashu <ithinker1991@gmail.com>
2018-11-28 18:32:27 +0800

    Add log timer

--- src/main/java/org/tron/core/db2/core/SnapshotRoot.java
+++ src/main/java/org/tron/core/db2/core/SnapshotRoot.java
@@ -3,13 +3,20 @@ package org.tron.core.db2.core;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Streams;
 import java.lang.ref.WeakReference;
+import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.spongycastle.util.encoders.Hex;
 import org.tron.core.db.common.WrappedByteArray;
 import org.tron.core.db2.common.LevelDB;
@@ -26,17 +33,51 @@ public class SnapshotRoot extends AbstractSnapshot<byte[], byte[]> {
     db = new LevelDB(parentName, name);
     dbName = name;
     solidity = this;
+
+    ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+    service.scheduleWithFixedDelay(() -> {
+      try {
+        logger.error("db:" + dbName + " hit:" + hitMap.get(dbName) + " miss:"+missMap.get(dbName) + " access:" + accessMap.get(dbName));
+      } catch (Throwable t) {
+        logger.error("Exception in log worker", t);
+      }
+    }, 10, 10, TimeUnit.SECONDS);
   }
+
+  Map<String, Long> hitMap = new ConcurrentHashMap<>();
+  Map<String, Long> accessMap = new ConcurrentHashMap<>();
+  Map<String, Long> missMap = new ConcurrentHashMap<>();
 
   @Override
   public byte[] get(byte[] key) {
     byte[] val = db.get(key);
-    logger.error("db:" + dbName + " key:" + key.length + " val:" + val.length);
+    if (!ArrayUtils.isEmpty(key)) {
+      long valLen = ArrayUtils.isEmpty(val) ? 0 : val.length;
+      if (!hitMap.containsKey(dbName)) {
+        hitMap.put(dbName, 1L);
+        accessMap.put(dbName, 0L);
+        if (valLen == 0) {
+          missMap.put(dbName, valLen);
+        } else {
+          missMap.put(dbName, 0L);
+        }
+      } else {
+        hitMap.put(dbName, hitMap.get(dbName) + 1);
+        accessMap.put(dbName, accessMap.get(dbName) + valLen);
+        if (valLen == 0) {
+          missMap.put(dbName, missMap.get(dbName) + 1);
+        }
+      }
+//      logger.error("GET:" + dbName + " key:" + key.length + " val:" + (val == null ? "0 " : val.length));
+    }
     return val;
   }
 
   @Override
   public void put(byte[] key, byte[] value) {
+//    if (ArrayUtils.isNotEmpty(key)) {
+//      logger.error("PUT:" + dbName + " key:" + key.length + " val:" + (ArrayUtils.isEmpty(value) ? "0 " : value.length));
+//    }
     db.put(key, value);
   }
 

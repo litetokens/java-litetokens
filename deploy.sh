c569aebf25d8b4a55901ebc1b497e7155aaa3a54
Huang Zheng Si <huangzhengsi@tron.network>
2018-12-06 17:50:54 +0800

    test travis on multi os - without stest

--- deploy.sh
+++ deploy.sh
@@ -35,7 +35,8 @@ change_branch_CMD="sed -i '1c branch_name_in_CI=$TRAVIS_BRANCH' /data/workspace/
 
 echo "$TRAVIS_BRANCH"
 
-if [[ "$TRAVIS_BRANCH" = "develop" || "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "Odyssey_v3.2.1" ]];then
+#if [[ "$TRAVIS_BRANCH" = "develop" || "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "Odyssey_v3.2.1" ]];then
+if [[ "$TRAVIS_BRANCH" = "123" ]];then
   if [ "$TRAVIS_BRANCH" = "master" ];then
     change_branch_CMD="sed -i '1c branch_name_in_CI=Odyssey_v3.2.1' /data/workspace/docker_workspace/do_stest.sh"
   fi

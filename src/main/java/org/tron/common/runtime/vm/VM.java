38faabbdefae8ec28ae50bc9af7b7b7e492ba98b
tjchern <tjchern@pku.edu.cn>
2018-12-07 15:32:34 +0800

    add key log in sload and sstore

--- src/main/java/org/tron/common/runtime/vm/VM.java
+++ src/main/java/org/tron/common/runtime/vm/VM.java
@@ -126,7 +126,8 @@ public class VM {
           DataWord oldValue = program.storageLoad(stack.peek());
 
           PerformanceHelper.singleTxOpcodeInfo
-              .add("SLOAD_IN_SSTORE\1" + String.valueOf(System.nanoTime() / 1000 - preMs));
+              .add("SLOAD_IN_SSTORE\1" + stack.peek().toHexString() + "\1" + String
+                  .valueOf(System.nanoTime() / 1000 - preMs));
 
           if (oldValue == null && !newValue.isZero()) {
             // set a new not-zero value
@@ -1061,7 +1062,8 @@ public class VM {
           DataWord val = program.storageLoad(key);
 
           PerformanceHelper.singleTxOpcodeInfo
-              .add("SLOAD\1" + key.toHexString() + "\1" + String.valueOf(System.nanoTime() / 1000 - preMs));
+              .add("SLOAD\1" + key.toHexString() + "\1" + String
+                  .valueOf(System.nanoTime() / 1000 - preMs));
 
           if (logger.isDebugEnabled()) {
             hint = "key: " + key + " value: " + val;
@@ -1094,7 +1096,8 @@ public class VM {
           program.storageSave(addr, value);
 
           PerformanceHelper.singleTxOpcodeInfo
-              .add("SSTORE\1" + String.valueOf(System.nanoTime() / 1000 - preMs));
+              .add("SSTORE\1" + addr.toHexString() + "\1" + String
+                  .valueOf(System.nanoTime() / 1000 - preMs));
 
           program.step();
         }
